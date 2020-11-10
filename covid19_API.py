8684772b241467e5a4d156ee48bc5679a9598e25
Abhinav Abhishek <abhinav.abhishek@sykes.com>
2020-11-11 03:38:52 +0530

    make it better

--- covid19_API.py
+++ covid19_API.py
@@ -1,6 +1,7 @@
 import calendar
 
 from flask import Flask, request, make_response
+from flask_cors import CORS
 
 import json
 
@@ -106,4 +107,5 @@ def product_info():
     return ret_json
 
 if __name__ == '__main__':
+    CORS(app)
     app.run(debug=True)
